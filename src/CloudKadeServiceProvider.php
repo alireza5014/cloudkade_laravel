6a8930da9b48abb3029b920cd138a5857f801e89
alireza <persian402@gmail.com>
2022-09-20 12:16:45 +0430

    first commit

---
+++ src/CloudKadeServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+
+class CloudKadeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__ . '/routes.php';
+    }
+
+}
