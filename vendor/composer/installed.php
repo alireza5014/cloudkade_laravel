d6f1da8890edaab1b5320df5439810c1f2206f9e
alireza <persian402@gmail.com>
2022-09-20 12:53:18 +0430

    fix 1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '331c5214fd9422c27d9213a7ed6cb84386a6ae69',
+    'name' => 'cloudkade/cloudkade_laravel',
+  ),
+  'versions' => 
+  array (
+    'cloudkade/cloudkade_laravel' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '331c5214fd9422c27d9213a7ed6cb84386a6ae69',
+    ),
+  ),
+);
