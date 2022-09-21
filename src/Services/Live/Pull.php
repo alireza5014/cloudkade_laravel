e431a32c8475a714bda2dab479fbae5ef5a1c07d
alireza <persian402@gmail.com>
2022-09-21 15:34:14 +0430

    fix 001211

--- src/Services/Live/Pull.php
+++ src/Services/Live/Pull.php
@@ -8,13 +8,13 @@ class Pull extends Live
 
     public function start($stream_id)
     {
-        return parent::$http->delete("/live/streams/" . $stream_id . "/pull/start", $this->data);
+        return parent::$http->get("/live/streams/" . $stream_id . "/pull/start", $this->data);
 
     }
 
     public function stop($stream_id)
     {
-        return parent::$http->delete("/live/streams/" . $stream_id . "/pull/stop", $this->data);
+        return parent::$http->get("/live/streams/" . $stream_id . "/pull/stop", $this->data);
 
     }
 }
--- src/Services/Live/Record.php
+++ src/Services/Live/Record.php
@@ -8,19 +8,19 @@ class Record extends Live
 
     public function start($stream_id)
     {
-        return parent::$http->delete("/live/streams/" . $stream_id . "/record/start", $this->data);
+        return parent::$http->get("/live/streams/" . $stream_id . "/record/start", $this->data);
 
     }
 
     public function end($stream_id)
     {
-        return parent::$http->delete("/live/streams/" . $stream_id . "/record/end", $this->data);
+        return parent::$http->get("/live/streams/" . $stream_id . "/record/end", $this->data);
 
     }
 
     public function pause($stream_id)
     {
-        return parent::$http->delete("/live/streams/" . $stream_id . "/record/stop", $this->data);
+        return parent::$http->get("/live/streams/" . $stream_id . "/record/stop", $this->data);
 
     }
 }
